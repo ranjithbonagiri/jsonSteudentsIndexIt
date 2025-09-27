e717c16531d03ff71a9c48f453ccc2c54a8814e3
ranjith bonagiri <ranjith6125@gmail.com>
2025-09-27 05:39:28 +0000

    feat: json model implementation

--- webapp/controller/zStudents.controller.js
+++ webapp/controller/zStudents.controller.js
@@ -1,10 +1,20 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+
+], (Controller, JSONModel) => {
     "use strict";
 
     return Controller.extend("org.indexit.jsonstudents.controller.zStudents", {
         onInit() {
+            var jmodel = new JSONModel();
+            var studentData = {
+                "Name": "Rahul",
+                "Age": "15"
+
+            };
+            jmodel.setData(studentData);
+
         }
     });
 });
