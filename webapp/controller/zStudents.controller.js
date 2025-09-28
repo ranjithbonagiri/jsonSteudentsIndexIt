7b4d32601991ffc48e3af2b67aa70374e3c7b25c
Index It <indexitsapui5fiori@gmail.com>
2025-09-28 05:43:46 +0000

    feat: updation in json

--- webapp/controller/zStudents.controller.js
+++ webapp/controller/zStudents.controller.js
@@ -7,13 +7,37 @@ sap.ui.define([
 
     return Controller.extend("org.indexit.jsonstudents.controller.zStudents", {
         onInit() {
-            var jmodel = new JSONModel();
+           //create a data
             var studentData = {
-                "Name": "Rahul",
-                "Age": "15"
+    "Name": "Rahul",
+    "Age": "15",
+    "RollNo": "1000",
+    "Subjects": [
+        {
+            "English": "50",
+            "Philosophy": [
+                {
+                    "SpclLit": "100",
+                    "EnglishHonors": "120"
+                }
+            ]
+        },
+        {
+            "Maths": "150"
+        },
+        {
+            "Chemistry": "150"
+        }
+    ]
+};
 
-            };
+           
+
+            //create a json and fill the json
+             var jmodel = new JSONModel();
             jmodel.setData(studentData);
+
+            this.getView().setModel(jmodel,"jModel")
 
         }
     });
